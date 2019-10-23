940d85493a72125f691d87ce9f9ae062a7400601
Rand Asswad <rand.asswad@insa-rouen.fr>
2019-10-23 13:36:50 +0200

    Migrated to scrollify

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,33 @@
-// SMOOTH SCROLLING SECTIONS
-$('a[href*=\\#]:not([href=\\#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
+// activate first nav element
+$(".home-nav li:first").addClass("active");
+
+// activate scrollify plugin
+$.scrollify({
+    section : ".section",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function(i, sections) {
+        // change active li link
+        var href = sections[i].attr("data-section-name");
+        $(".home-nav li.active").removeClass("active");
+        $(".home-nav li").find("a[href=\"#" + href + "\"]").parent().addClass("active");
+    },
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+});
+
+// activate nav links
+$(".home-nav a").on("click", function() {
+    $.scrollify.move($(this).attr("href"));
 });
