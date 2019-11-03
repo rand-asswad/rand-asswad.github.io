53888b8ea3a06dd8449ed4e4c73fbc31041aae35
Rand Asswad <rand.asswad@insa-rouen.fr>
2019-11-03 16:12:31 +0100

    Handle multiple tab instances at once

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,40 +1,44 @@
+const tabInstances = document.querySelectorAll(".tabs-container");
 
-const tabs = document.querySelectorAll('.tabs .tab');
+for (let t = 0; t < tabInstances.length; t++) {
+  const tabId = '#' + tabInstances[t].id + ' ';
+  const tabs = document.querySelectorAll(tabId + '.tabs .tab');
 
-// init active tab
-if (document.querySelector('.tabs .active') === null) {
-  if (tabs.length > 0) {
-    tabs[0].classList.add('active');
-    var contentId = tabs[0].getAttribute("href");
-    var content = document.getElementById(contentId.substr(1));
-    // remove active tab contents (just in case)
-    var tabContents = document.querySelectorAll('.tabs-content .active');
-    for (let i = 0; i < tabContents.length; i++) {
-      tabContents[i].classList.remove('active');
+  // init active tab
+  if (document.querySelector(tabId + '.tabs .active') === null) {
+    if (tabs.length > 0) {
+      tabs[0].classList.add('active');
+      var contentId = tabs[0].getAttribute("href");
+      var content = document.getElementById(contentId.substr(1));
+      // remove active tab contents (just in case)
+      var tabContents = document.querySelectorAll(tabId + '.tabs-content .active');
+      for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active');
+      }
+      // make first tab contents active
+      content.classList.add('active');
     }
-    // make first tab contents active
-    content.classList.add('active');
   }
-}
 
-// handle tab clicks
-for (let i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener('click', e => handleClick(e));
-}
+  // handle tab clicks
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', e => handleClick(e));
+  }
 
-function handleClick(e) {
-  e.preventDefault();
-  e.stopImmediatePropagation(); // since prevent default didn't work
-  const tab = e.target;
-  const content = tab.getAttribute("href").substr(1);
+  function handleClick(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation(); // since prevent default didn't work
+    const tab = e.target;
+    const content = tab.getAttribute("href").substr(1);
 
-  // clear active tab
-  document.querySelector('.tabs .active').classList.remove('active');
-  document.querySelector('.tabs-content .active').classList.remove('active');
+    // clear active tab
+    document.querySelector(tabId + '.tabs .active').classList.remove('active');
+    document.querySelector(tabId + '.tabs-content .active').classList.remove('active');
 
-  // select new tab
-  tab.classList.add('active');
-  document.getElementById(content).classList.add('active');
+    // select new tab
+    tab.classList.add('active');
+    document.getElementById(content).classList.add('active');
 
-  return false;
+    return false;
+  }
 }
